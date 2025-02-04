1b5b6e721d736b7154f4231d9b837639525f6bd7
alinnecaastro <alinnecastro133@gmail.com>
2025-02-04 15:31:34 -0300

    atualização do diagrama

--- temporizador_one_shot.c
+++ temporizador_one_shot.c
@@ -11,6 +11,7 @@ bool led_red_active = false;
 bool led_azul_active = false;
 bool led_green_active = false;
 bool sequence_active = false; // Flag para indicar se a sequência está ativa
+int elapsed_time = 1; 
 
 // Função de callback para mudar o estado dos LEDs
 int64_t change_led_state_callback(alarm_id_t id, void *user_data) {
@@ -18,18 +19,18 @@ int64_t change_led_state_callback(alarm_id_t id, void *user_data) {
         // Desliga o LED vermelho após 3 segundos
         gpio_put(LED_AZUL, false);
         led_red_active = false;
-        printf("Dois LEDs ligados (Azul e Verde)\n");
+        printf("Tempo: %d segundos | Dois LEDs ligados (Azul e Verde,)\n",elapsed_time);
     } else if (!led_red_active && led_azul_active && led_green_active) {
         // Desliga o LED azul após mais 3 segundos
         gpio_put(LED_RED, false);
         led_azul_active = false;
-        printf("Apenas um LED ligado (Verde)\n");
+        printf("Tempo: %d segundos | Apenas um LED ligado (Verde)\n",elapsed_time);
     } else if (!led_red_active && !led_azul_active && led_green_active) {
         // Desliga o LED verde após mais 3 segundos
         gpio_put(LED_GREEN, false);
         led_green_active = false;
         sequence_active = false; // Finaliza a sequência
-        printf("Todos os LEDs desligados\n");
+        printf("Tempo: %d segundos |Todos os LEDs desligados\n",elapsed_time);
     }
     return 0; // Retorna 0 para indicar que o alarme não deve ser repetido
 }
@@ -63,8 +64,8 @@ int main() {
                 led_azul_active = true;
                 led_green_active = true;
                 sequence_active = true; // Indica que a sequência está ativa
-
-                printf("Todos os LEDs ligados\n");
+                elapsed_time = 1;
+                printf("Tempo: %d segundos |Todos os LEDs ligados\n",elapsed_time);
 
                 // Agenda a primeira mudança de estado após 3 segundos
                 add_alarm_in_ms(3000, change_led_state_callback, NULL, false);
@@ -74,8 +75,14 @@ int main() {
                 add_alarm_in_ms(9000, change_led_state_callback, NULL, false);
             }
         }
-
-        sleep_ms(10); // Pequeno atraso para evitar leitura múltipla do botão
+ // Incrementa o tempo decorrido a cada segundo
+        if (sequence_active) {
+            sleep_ms(1000); // Espera 1 segundo
+            elapsed_time++; // Incrementa o tempo decorrido
+        } else {
+            sleep_ms(10); // Pequeno atraso para evitar leitura múltipla do botão
+        }
+     // Pequeno atraso para evitar leitura múltipla do botão
     }
 
     return 0;
